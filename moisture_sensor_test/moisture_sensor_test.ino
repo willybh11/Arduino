08318f1d4ddabd48ddb661307dec79e9dcce9eff
willybh11 <willhagele@gmai.com>
2019-11-17 16:15:47 -0500

    Edit moisture test

--- moisture_sensor_test/moisture_sensor_test.ino
+++ moisture_sensor_test/moisture_sensor_test.ino
@@ -1,12 +1,16 @@
 
 void setup() {
   Serial.begin(9600);
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Moisture Sensor Value:");
-  Serial.println(analogRead(A0));
-  delay(100);
+  Serial.print("Sensor 1:");
+  Serial.println(analogRead(A1));
+  Serial.print("Sensor 2:");
+  Serial.println(analogRead(A2));
+  Serial.print("Sensor 3:");
+  Serial.println(analogRead(A3));
+  Serial.println();
+  delay(500);
 }
